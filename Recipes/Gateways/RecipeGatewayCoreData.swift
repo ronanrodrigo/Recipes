2d6b70a0f6643e881137675e63c1ab40426c7fdc
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-02-09 08:53:12 -0200

    Implementing iOS side

---
+++ Recipes/Gateways/RecipeGatewayCoreData.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeGatewayCoreData.swift
+//  Recipes
+//
+//  Created by Ronan Rodrigo Nunes on 2/9/16.
+//  Copyright © 2016 Ronan Rodrigo Nunes. All rights reserved.
+//
+
+import Foundation
+
+class RecipeGatewayCoreData: RecipeGateway {
+  func create(recipe: Recipe) -> Recipe {
+    return recipe
+  }
+}
