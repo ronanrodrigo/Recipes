2fea017cd483675980dac5d246b650a449aa8663
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-02-10 20:31:53 -0200

    Bum

--- Recipes.playground/Pages/Intro.xcplaygroundpage/Contents.swift
+++ Recipes.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -1,3 +1,18 @@
 import Foundation
 
-var str = "Hello, playground"
+extension Int
+{
+  public static func random(range: Range<Int>) -> Int {
+    return Int(arc4random_uniform(UInt32(range.endIndex - range.startIndex))) + range.startIndex
+  }
+}
+
+var x = 0
+for n in 1900...2000 {
+  if n % 2 == 0 {
+    x = n / Int.random(1...1000)
+  } else {
+    x = n * Int.random(1...1000)
+  }
+  x
+}
