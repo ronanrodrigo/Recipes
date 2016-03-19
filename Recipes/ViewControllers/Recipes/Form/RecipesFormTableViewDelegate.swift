dff9c10b594a2e0d173a81c8b2718ce181f2d436
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-03-19 11:36:12 -0300

    RecipesFormTableViewDelegate

---
+++ Recipes/ViewControllers/Recipes/Form/RecipesFormTableViewDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  RecipesFormDelegate.swift
+//  Recipes
+//
+//  Created by Ronan Rodrigo Nunes on 19/03/16.
+//  Copyright © 2016 Ronan Rodrigo Nunes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipesFormTableViewDelegate: NSObject, UITableViewDelegate {
+    var delegate: RecipesFormControllerDelegate
+    
+    init(delegate: RecipesFormControllerDelegate) {
+        self.delegate = delegate
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.delegate.didTapeedAtAddIngredients()
+    }
+}
