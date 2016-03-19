d037aa4a3259bd4c626cb701f8ace54d29c84746
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-03-19 13:22:54 -0300

    Revert "Testing pods commmited"
    This reverts commit bac7308608335b3303093433416504cd6c3185d1.

--- Pods/Target Support Files/Pods-Recipes/Pods-Recipes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Recipes : NSObject
-@end
-@implementation PodsDummy_Pods_Recipes
-@end
--- Pods/Target Support Files/Pods-Recipes/Pods-Recipes-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RecipesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RecipesVersionString[];
-
